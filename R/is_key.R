e1ec6e734e6c1ecf3cf3facdfec5cc4bcb66cfc0
dsander04 <129232282+dsander04@users.noreply.github.com>
2025-12-02 16:27:05 -0600

    Documentation update for is_key

--- R/is_key.R
+++ R/is_key.R
@@ -1,4 +1,15 @@
-is.key <- function(data, ..., optional_arguement = FALSE) {
+#' Assess whether a proposed (set of) variable(s) is a key
+#'
+#' @param data data frame or tibble
+#' @param ... variables to be considered as a key
+#' @param optional_argument binary, defaluts to `FALSE`
+#' @returns logical value: `TRUE`, if the specified variable (combination) is a key,
+#' `FALSE` otherwise
+#' @examples
+#' @examples
+#' data(college)
+#' is_key(college, name)
+is_key <- function(data, ..., optional_arguement = FALSE) {
   key_candidate <- data |> select(...)
   !any(duplicated(key_candidate))
 }
